a410dbf5a6ab556709d4262fe9b666eedab107de
José Ángel García <chepeaicrag12@gmail.com>
2021-11-11 13:38:44 -0600

    Configuracion de archivos

--- Backend/huxgym/huxgym/asgi.py
+++ Backend/huxgym/huxgym/asgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+from decouple import config
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'huxgym.settings')
+if config('DEVELOPMENT'):
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'huxgym.settings.local')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'huxgym.settings.production')
 
 application = get_asgi_application()
--- Backend/huxgym/huxgym/wsgi.py
+++ Backend/huxgym/huxgym/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+from decouple import config
+from .settings import *
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'huxgym.settings')
+if config('DEVELOPMENT'):
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'huxgym.settings.local')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'huxgym.settings.production')
 
 application = get_wsgi_application()
